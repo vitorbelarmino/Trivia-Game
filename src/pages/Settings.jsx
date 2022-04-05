d18f63a9aab96af3b5b9a190207410926d61cc78
Vitor <vitor.belarmino@hotmail.com>
2022-04-05 19:25:35 -0300

    requisito 3 completo

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+  render() {
+    return (
+      <h2 data-testid="settings-title">Configurações</h2>
+    );
+  }
+}
+
+export default Settings;
