38a26b0a46c9519eb983521e84f62d7fa22c0e8b
Jéssica Florentino <jdgflorentino@gmail.com>
2022-04-06 09:17:05 -0300

    [Grupo 4]- Requisito 4 completo

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,11 @@
 export const ADD_TOKEN = 'ADD_TOKEN';
+export const SAVE_INFOS = 'SAVE_INFOS';
 
 export const tokenAction = (value) => ({ type: ADD_TOKEN, payload: value });
+
+export const saveInfos = (email, name) => (
+  {
+    type: SAVE_INFOS,
+    email,
+    name,
+  });
