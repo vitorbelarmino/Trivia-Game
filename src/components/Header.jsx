a9eaf38c3b3cd9f5044d38556a88cdfbaa7d8bfe
Jéssica Florentino <jdgflorentino@gmail.com>
2022-04-06 09:26:11 -0300

    [Grupo 4] - Requisito 4 - completo.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,6 +10,7 @@ class Header extends React.Component {
     return (
       <div>
         <header>
+          <p>Header</p>
           <img
             src={ gravatar }
             alt="imagem player"
