af81ab20729e63429dcb29b04461f0e7da6f4da1
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-04-08 17:50:47 -0300

    muda indexOf pro estado global

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ export const ADD_TOKEN = 'ADD_TOKEN';
 export const SAVE_INFOS = 'SAVE_INFOS';
 export const SAVE_SCORE = 'SAVE_SCORE';
 export const SAVE_ASSERTIONS = 'SAVE_ASSERTIONS';
+export const INDEX_OF = 'INDEX_OF';
 
 export const tokenAction = (value) => ({ type: ADD_TOKEN, payload: value });
 
@@ -16,3 +17,5 @@ export const saveInfos = (email, name, image) => (
 export const saveScore = (value) => ({ type: SAVE_SCORE, payload: value });
 
 export const saveAssertions = (value) => ({ type: SAVE_ASSERTIONS, payload: value });
+
+export const indexOfAction = () => ({ type: INDEX_OF });
