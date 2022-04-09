afa6e0dd6fadd86d5ce013162571d1342a5285de
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-04-09 12:42:21 -0300

    adiciona estilo na pagina de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,8 @@ import { Redirect, withRouter } from 'react-router-dom';
 import md5 from 'crypto-js/md5';
 import { tokenAction, saveInfos } from '../actions';
 import fetchToken from '../server';
+import '../styles/Login.css';
+import trivia from '../assets/trivia.png';
 
 class Login extends Component {
   constructor() {
@@ -12,7 +14,7 @@ class Login extends Component {
     this.state = {
       email: '',
       name: '',
-      isDisabled: true,
+      isDisabled: false,
       isRedirect: false,
     };
   }
@@ -22,9 +24,9 @@ class Login extends Component {
     const isNameValid = name.length > 0;
     const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email); // ref. https://github.com/tryber/sd-019-c-live-lectures/blob/lecture/11.5/aula_extra/src/App.js
     if (isNameValid && isEmailValid) {
-      this.setState({ isDisabled: false });
-    } else {
       this.setState({ isDisabled: true });
+    } else {
+      this.setState({ isDisabled: false });
     }
   }
 
@@ -49,9 +51,11 @@ class Login extends Component {
   render() {
     const { email, name, isDisabled, isRedirect } = this.state;
     return (
-      <>
-        <h1>Trivia</h1>
-        <form>
+      <div className="container-login">
+        <h1 className="title-login">
+          <img src={ trivia } alt="trivia-logo" />
+        </h1>
+        <form className="form-container">
 
           <label htmlFor="email">
             Email:
@@ -79,6 +83,7 @@ class Login extends Component {
 
           <button
             type="button"
+            className="btn-play-login"
             data-testid="btn-play"
             disabled={ isDisabled }
             onClick={ this.handleClick }
@@ -88,6 +93,7 @@ class Login extends Component {
         </form>
         <button
           type="button"
+          className="settings-login"
           data-testid="btn-settings"
           onClick={ this.redirectClick }
         >
@@ -95,7 +101,7 @@ class Login extends Component {
 
         </button>
         { isRedirect && <Redirect to="/settings" /> }
-      </>
+      </div>
     );
   }
 }
